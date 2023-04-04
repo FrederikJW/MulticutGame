98bfd38a8303acc66e5db069a8bf8658164dc816
FrederikJW <frederik.weber@mailbox.tu-dresden.de>
2023-04-04 23:44:55 +0200

    designing menu

--- MulticutGame.py
+++ MulticutGame.py
@@ -2,6 +2,11 @@ import pygame
 import sys
 
 from button import Button
+from enum import Enum
+
+
+class GameMode(Enum):
+    Classic = 1
 
 
 class MulticutGame:
@@ -12,6 +17,7 @@ class MulticutGame:
 
     def __init__(self):
         pygame.init()
+        self.clock = pygame.time.Clock()
 
         # Set up font
         self.font = pygame.font.SysFont('Ariel', 32)
@@ -29,24 +35,35 @@ class MulticutGame:
         self.init_buttons()
 
     def init_buttons(self):
-        self.buttons.append(Button('Quit', 0, 0, 100, 50, (255, 0, 0), (0, 255, 0), self.quit))
+        self.buttons.append(Button('Level1', 50, 190, 200, 40, 'blue', None))
+        self.buttons.append(Button('Level2', 50, 240, 200, 40, 'blue', None))
+        self.buttons.append(Button('Level3', 50, 290, 200, 40, 'blue', None))
+        self.buttons.append(Button('Level4', 50, 340, 200, 40, 'blue', None))
+        self.buttons.append(Button('Level5', 50, 390, 200, 40, 'blue', None))
+        self.buttons.append(Button('Level6', 50, 440, 200, 40, 'blue', None))
+        self.buttons.append(Button('Level7', 50, 490, 200, 40, 'blue', None))
+        self.buttons.append(Button('Level8', 50, 540, 200, 40, 'blue', None))
+        self.buttons.append(Button('Level9', 50, 590, 200, 40, 'blue', None))
+        self.buttons.append(Button('Quit', 50, 640, 200, 40, 'red', self.quit))
 
     def run(self):
         while True:
+            mouse_pos = pygame.mouse.get_pos()
             # Handle events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.quit()
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    mouse_pos = pygame.mouse.get_pos()
                     for button in self.buttons:
                         if button.collides(mouse_pos):
                             button.action()
 
             # draw buttons
             for button in self.buttons:
+                button.hover(button.collides(mouse_pos))
                 self.screen.blit(*button.objects())
 
+            self.clock.tick(30)
             pygame.display.update()
 
     def quit(self):
--- button.py
+++ button.py
@@ -1,26 +1,63 @@
 import pygame
 
+RED = (234, 76, 137)  # ea4c89
+DARK_RED = (205, 24, 94)
+BLUE = (0, 140, 186)  # 008CBA
+DARK_BLUE = (0, 115, 153)
+GREEN = (76, 175, 80)  # 4CAF50
+DARK_GREEN = (62, 142, 65)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
 
 class Button:
-    def __init__(self, label, x, y, width, height, fill_color, outline_color, action_func=None):
-        self.label = label
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.fill_color = fill_color
-        self.outline_color = outline_color
+    def __init__(self, label, x, y, width, height, color_style, action_func=None):
+        if color_style == 'red':
+            self.background_color = RED
+            self.hover_color = WHITE
+            self.border_color = DARK_RED
+        elif color_style == 'green':
+            self.background_color = GREEN
+            self.hover_color = WHITE
+            self.border_color = DARK_GREEN
+        else:
+            self.background_color = BLUE
+            self.hover_color = WHITE
+            self.border_color = DARK_BLUE
+        self.text_color = WHITE
+
         self.action_func = action_func
+        self.hovers = False
+        self.label = label
 
         self.font = pygame.font.SysFont('Ariel', 32)
-        self.text_surface = self.font.render(label, True, (0, 0, 0))
+        self.text_surface = self.font.render(label, True, self.text_color)
         self.text_rec = self.text_surface.get_rect()
-        self.text_rec.center = ((self.x+(self.width/2)), (self.y+(self.height/2)))
+        self.text_rec.center = (width/2, height/2)
 
-        self.surface = pygame.Surface((self.width, self.height))
-        self.surface.fill(self.fill_color)  # white color
+        self.surface = pygame.Surface((width, height))
+        self.surface.fill(WHITE)
+        self.rec = pygame.Rect(x, y, width, height)
+        self.draw(self.background_color, self.border_color, self.text_color)
+
+    def draw(self, fill_color, border_color, text_color):
+        self.text_surface = self.font.render(self.label, True, text_color)
+        rec = self.surface.get_rect()
+        center = rec.center
+        pygame.draw.rect(self.surface, border_color, rec, border_radius=10)
+        rec.width = rec.width - 4
+        rec.height = rec.height - 4
+        rec.center = center
+        pygame.draw.rect(self.surface, fill_color, rec, border_radius=8)
         self.surface.blit(self.text_surface, self.text_rec)
-        self.rec = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def hover(self, hovers):
+        if hovers and not self.hovers:
+            self.hovers = True
+            self.draw(self.hover_color, self.border_color, BLACK)
+        elif not hovers and self.hovers:
+            self.hovers = False
+            self.draw(self.background_color, self.border_color, self.text_color)
 
     def objects(self):
         return self.surface, self.rec
@@ -29,6 +66,5 @@ class Button:
         return self.rec.collidepoint(pos)
 
     def action(self):
-        if self.action_func is None:
-            return
-        self.action_func()
+        if self.action_func is not None:
+            self.action_func()
