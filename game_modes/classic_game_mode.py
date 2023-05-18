33a0a08add00604e9fb2e1e2e1e87c2d3a061f84
FrederikJW <frederik.weber@mailbox.tu-dresden.de>
2023-05-18 16:19:13 +0200

    working on game_mode to prepare for walkthrough_game_mode

--- game_modes/classic_game_mode.py
+++ game_modes/classic_game_mode.py
@@ -1,13 +1,9 @@
 import gettext
 import os
 
-import pygame
-
-import colors
 import constants
 from button import Button
 from graph import GraphFactory
-from utils import sub_pos
 from .game_mode import GameMode
 
 localedir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'locale')
@@ -21,109 +17,39 @@ class ClassicGameMode(GameMode):
     def __init__(self, graph_type, graph_width=4, graph_height=4):
         super().__init__()
 
-        self.graph = None
         self.graph_type = graph_type
         self.graph_width = graph_width
         self.graph_height = graph_height
         if graph_type == 'grid':
-            self.graph = GraphFactory.generate_grid((self.graph_width, self.graph_height))
+            self.graphs.update({0: GraphFactory.generate_grid((self.graph_width, self.graph_height))})
         elif graph_type == 'pentagram':
-            self.graph = GraphFactory.generate_pentagram()
-        self.score_drawn = False
-
-        self.move_vertex = None
-        self.graph_rel_offset = sub_pos(constants.GRAPH_OFFSET, constants.GAME_MODE_SCREEN_OFFSET)
-
-        self.draw_necessary = True
+            self.graphs.update({0: GraphFactory.generate_pentagram()})
+        self.active_graph = self.graphs[0]
 
         # init buttons
-        self.buttons = []
         margin_top = constants.MARGIN
         margin_right = constants.MARGIN
         size = (200, 40)
         pos_x = constants.GAME_MODE_SCREEN_SIZE[0] - margin_right - size[0]
-        self.buttons.extend([
-            Button(_('Reset'), (pos_x, margin_top), size, 'red', self.reset_graph, constants.GAME_MODE_HEAD_OFFSET),
-            Button(_('Regenerate'), (pos_x, margin_top + 50), size, 'red', self.regenerate_graph, constants.GAME_MODE_HEAD_OFFSET)
-        ])
+        self.buttons.append(Button(_('Regenerate'), (pos_x, margin_top + 50), size, 'red', self.regenerate_graph,
+                                   constants.GAME_MODE_HEAD_OFFSET))
 
     def regenerate_graph(self):
         if self.graph_type == 'grid':
-            self.graph = GraphFactory.generate_grid((self.graph_width, self.graph_height))
+            self.graphs.update({0: GraphFactory.generate_grid((self.graph_width, self.graph_height))})
         elif self.graph_type == 'pentagram':
-            self.graph = GraphFactory.generate_pentagram()
+            self.graphs.update({0: GraphFactory.generate_pentagram()})
+        self.active_graph = self.graphs[0]
         self.score_drawn = False
-
-    def reset_graph(self):
-        self.graph.reset()
 
     def switch_to(self):
         pass
 
-    def print_score(self):
-        score_surface = self.font.render(_('Score') + f" = {self.graph.get_score()}", True, colors.BLACK)
-        score_rec = score_surface.get_rect().move((constants.MARGIN, constants.MARGIN))
-        optimal_score = self.graph.optimal_score
-        if optimal_score is None:
-            optimal_score = _("calculating")
-        else:
-            optimal_score = str(int(optimal_score))
-        optimal_score_surface = self.font.render(_('Optimal Score') + f" = {optimal_score}", True, colors.BLACK)
-        optimal_score_rec = optimal_score_surface.get_rect().move((constants.MARGIN, constants.MARGIN + 50))
-
-        self.head_surface.blit(score_surface, score_rec)
-        self.head_surface.blit(optimal_score_surface, optimal_score_rec)
-
     def draw(self):
-        self.graph.draw(self.highlight_group)
-        self.print_score()
-        self.body_surface.blit(*self.graph.objects())
+        pass
 
-    def run(self, events):
-        self.highlight_group = None
-
-        mouse_pos = pygame.mouse.get_pos()
-        if not pygame.Rect(constants.GAME_MODE_BODY_OFFSET, self.graph.rec.size).collidepoint(mouse_pos):
-            self.move_vertex = None
-        graph_mouse_pos = sub_pos(mouse_pos, constants.GAME_MODE_BODY_OFFSET)
-        gamemode_mouse_pos = sub_pos(mouse_pos, constants.GAME_MODE_SCREEN_OFFSET)
-
-        if not self.score_drawn and self.graph.optimal_score is not None:
-            self.draw_necessary = True
-
-        for event in events:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                for vertex in self.graph.vertices.values():
-                    if vertex.rec.collidepoint(graph_mouse_pos):
-                        self.move_vertex = vertex
-                        if len(self.move_vertex.group.vertices) > 1:
-                            self.graph.move_vertex_to_group(self.move_vertex, None)
-                        break
-                for button in self.buttons:
-                    if button.collides(gamemode_mouse_pos):
-                        button.action()
-                        self.draw_necessary = True
-            elif event.type == pygame.MOUSEBUTTONUP:
-                if self.move_vertex is not None:
-                    for group in self.graph.groups:
-                        if group != self.move_vertex.group and group.rec.colliderect(self.move_vertex.group.rec):
-                            self.graph.move_vertex_to_group(self.move_vertex, group)
-                            self.draw_necessary = True
-                            break
-                self.move_vertex = None
-
-        if self.move_vertex is not None:
-            self.move_vertex.move(graph_mouse_pos)
-            for group in self.graph.groups:
-                if group != self.move_vertex.group and group.rec.colliderect(self.move_vertex.group.rec):
-                    self.highlight_group = group
-                    break
-
-            self.draw_necessary = True
-
-        for button in self.buttons:
-            if button.hover(button.collides(gamemode_mouse_pos)):
-                self.draw_necessary = True
+    def run(self):
+        pass
 
     def exit(self):
         pass
--- game_modes/game_mode.py
+++ game_modes/game_mode.py
@@ -1,10 +1,19 @@
 import abc
-import constants
-from utils import sub_pos
+import gettext
+import os
 
 import pygame
 
 import colors
+import constants
+from button import Button
+from utils import sub_pos
+
+localedir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'locale')
+de = gettext.translation('base', localedir, languages=['de'])
+de.install()
+
+_ = de.gettext
 
 
 # abstract game mode class
@@ -23,12 +32,55 @@ class GameMode(metaclass=abc.ABCMeta):
         self.surface.fill(colors.COLOR_KEY)
         self.surface.set_colorkey(colors.COLOR_KEY)
 
+        self.mouse_pos = (0, 0)
+        self.graph_mouse_pos = (0, 0)
+        self.game_mode_mouse_pos = (0, 0)
+
         self.rec = pygame.Rect(*constants.GAME_MODE_SCREEN_OFFSET, *constants.GAME_MODE_SCREEN_SIZE)
 
-        self.buttons = []
+        self.graphs = {}
+        self.active_graph = None
+        self.score_drawn = False
+        self.move_vertex = None
+        self.highlight_group = None
 
         self.font = pygame.font.SysFont('Ariel', 32)
-        self.draw_necessary = False
+
+        self.draw_necessary = True
+
+        # init buttons
+        margin_top = constants.MARGIN
+        margin_right = constants.MARGIN
+        size = (200, 40)
+        pos_x = constants.GAME_MODE_SCREEN_SIZE[0] - margin_right - size[0]
+        self.buttons = []
+        self.buttons.append(
+            Button(_('Reset'), (pos_x, margin_top), size, 'red', self.reset_graph, constants.GAME_MODE_HEAD_OFFSET))
+
+    def reset_graph(self):
+        if self.active_graph is not None:
+            self.active_graph.reset()
+
+    def print_score(self):
+        if self.active_graph is not None:
+            score = self.active_graph.get_score()
+            optimal_score = self.active_graph.optimal_score
+            if optimal_score is None:
+                optimal_score = _("calculating")
+            else:
+                optimal_score = str(round(optimal_score))
+        else:
+            score = ''
+            optimal_score = ''
+
+        score_surface = self.font.render(_('Score') + f" = {score}", True, colors.BLACK)
+        score_rec = score_surface.get_rect().move((constants.MARGIN, constants.MARGIN))
+
+        optimal_score_surface = self.font.render(_('Optimal Score') + f" = {optimal_score}", True, colors.BLACK)
+        optimal_score_rec = optimal_score_surface.get_rect().move((constants.MARGIN, constants.MARGIN + 50))
+
+        self.head_surface.blit(score_surface, score_rec)
+        self.head_surface.blit(optimal_score_surface, optimal_score_rec)
 
     def objects(self):
         return self.surface, self.rec
@@ -44,17 +96,78 @@ class GameMode(metaclass=abc.ABCMeta):
         for button in self.buttons:
             self.head_surface.blit(*button.objects())
 
+        if self.active_graph is not None:
+            self.active_graph.draw(self.highlight_group)
+            self.body_surface.blit(*self.active_graph.objects())
+        self.print_score()
+
         self.draw()
 
         self.surface.blit(self.head_surface, constants.GAME_MODE_HEAD_RELATIVE_OFFSET)
         self.surface.blit(self.body_surface, constants.GAME_MODE_BODY_RELATIVE_OFFSET)
 
         # draw border
-        rec = pygame.Rect(sub_pos(constants.GAME_MODE_BODY_RELATIVE_OFFSET, (0, 2)), (constants.GAME_MODE_SCREEN_SIZE[0], 2))
+        rec = pygame.Rect(sub_pos(constants.GAME_MODE_BODY_RELATIVE_OFFSET, (0, 2)),
+                          (constants.GAME_MODE_SCREEN_SIZE[0], 2))
         pygame.draw.rect(self.surface, colors.GREY, rec)
 
+    def mouse_down_event(self):
+        for button in self.buttons:
+            if button.collides(self.game_mode_mouse_pos):
+                button.action()
+                self.draw_necessary = True
+
+        if self.active_graph is None:
+            return
+
+        for vertex in self.active_graph.vertices.values():
+            if vertex.rec.collidepoint(self.graph_mouse_pos):
+                self.move_vertex = vertex
+                if len(self.move_vertex.group.vertices) > 1:
+                    self.active_graph.move_vertex_to_group(self.move_vertex, None)
+                break
+
+    def mouse_up_event(self):
+        if self.move_vertex is not None and self.active_graph is not None:
+            for group in self.active_graph.groups:
+                if group != self.move_vertex.group and group.rec.colliderect(self.move_vertex.group.rec):
+                    self.active_graph.move_vertex_to_group(self.move_vertex, group)
+                    self.draw_necessary = True
+                    break
+        self.move_vertex = None
+
     def main(self, events):
-        self.run(events)
+        self.highlight_group = None
+
+        self.mouse_pos = pygame.mouse.get_pos()
+        if not pygame.Rect(constants.GAME_MODE_BODY_OFFSET, self.body_surface.get_size()).collidepoint(self.mouse_pos):
+            self.move_vertex = None
+        self.graph_mouse_pos = sub_pos(self.mouse_pos, constants.GAME_MODE_BODY_OFFSET)
+        self.game_mode_mouse_pos = sub_pos(self.mouse_pos, constants.GAME_MODE_SCREEN_OFFSET)
+
+        if self.active_graph is not None and not self.score_drawn and self.active_graph.optimal_score is not None:
+            self.draw_necessary = True
+
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.mouse_down_event()
+            elif event.type == pygame.MOUSEBUTTONUP:
+                self.mouse_up_event()
+
+        if self.move_vertex is not None and self.active_graph is not None:
+            self.move_vertex.move(self.graph_mouse_pos)
+            for group in self.active_graph.groups:
+                if group != self.move_vertex.group and group.rec.colliderect(self.move_vertex.group.rec):
+                    self.highlight_group = group
+                    break
+
+            self.draw_necessary = True
+
+        for button in self.buttons:
+            if button.hover(button.collides(self.game_mode_mouse_pos)):
+                self.draw_necessary = True
+
+        self.run()
 
         self._draw_wrapper()
 
@@ -67,7 +180,7 @@ class GameMode(metaclass=abc.ABCMeta):
         pass
 
     @abc.abstractmethod
-    def run(self, events):
+    def run(self):
         pass
 
     @abc.abstractmethod
