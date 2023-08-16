af7ab27860eef20b903020bc59ec273941b55c0f
FrederikJW <frederik.weber@mailbox.tu-dresden.de>
2023-08-16 11:18:18 +0200

    fixed bug in greedy joining

--- game_modes/greedy_joining.py
+++ game_modes/greedy_joining.py
@@ -21,10 +21,14 @@ class GreedyJoining(GameMode):
         self.headline_change_timestamp = None
         self.draw_necessary = True
         self.free_mode = False
+        self.change_all_buttons('show')
+        self.change_all_buttons('activate')
+        self.buttons['reset2'].hide()
 
     def reset_graph(self, one_group=False):
         self.standard_headline = 'In greedy joining you join those groups that improve your score the most. Try it!'
         self.headline = self.standard_headline
+        self.free_mode = False
         super().reset_graph(one_group)
 
     def graph_solved_event(self):
@@ -38,17 +42,17 @@ class GreedyJoining(GameMode):
 
     def run(self):
         if self.active_graph.has_changed:
-            best_score_improvement = self.active_graph.prev_state.get_best_score_improvement()
             if self.active_graph.get_score() == self.active_graph.optimal_score:
                 self.active_graph.deactivated = True
                 self.headline = 'Great! You solved it.'
-            elif best_score_improvement == 0:
+            elif self.active_graph.get_best_score_improvement() == 0:
                 self.standard_headline = ('Great! You followed the algorithm correctly and there is no more joining '
                                           'move left that could improve your score. Note that in this case the '
                                           'algorithm could not find the optimal solution. Can you find it?')
                 self.headline = self.standard_headline
                 self.headline_change_timestamp = time.time()
                 self.free_mode = True
+                self.draw_necessary = True
             elif ((not self.free_mode) and
                   self.active_graph.get_score() - self.active_graph.prev_state.get_score() >
                   self.active_graph.prev_state.get_best_score_improvement()):
